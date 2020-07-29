88744a1ab58e8eb5c50243fd27d1c1c3b6ea24ab
Debsankha Manik <dmanik@gwdg.de>
2020-07-29 19:25:00 +0200

    added some TODO's

--- thesimulator/simulator.py
+++ thesimulator/simulator.py
@@ -9,6 +9,16 @@ RequestResponse = Union[RequestAcceptanceEvent, RequestRejectionEvent]
 Event = Union[RequestAcceptanceEvent, RequestRejectionEvent, PickupEvent, DeliveryEvent]
 SingleVehicleSolution = Tuple[int, float, List[Stop]]  # vehicle_id, cost, new_stop_list
 
+# Thoughts
+# UserRequest vs InternalRequest. Only the latter has more time windows etc.
+# How to handle traveltime computing?
+# Who interpolates current vehicle position?
+# Who/where is CPE added?
+# If fleet state is stored distributedly, how does simulator visit stops?
+#  - It seems avoiding data copying is too complex/limiting.
+# Alternate ideas:
+# - Maintain FleetState as a Cython class -> No data copy.
+# - handle_request maintains a process pool.
 
 def handle_request_single_vehicle(req: Request, stoplist: List[Stop]) -> SingleVehicleSolution:
     """
@@ -24,19 +34,21 @@ def handle_request_single_vehicle(req: Request, stoplist: List[Stop]) -> SingleV
 
 
 def handle_request(req: Request, fleet_state: FleetState) -> RequestResponse:
-    # This is where all sorts of paralleization can be done, by replacing the map with
+    # This is where all sorts of parallelization can be done, by replacing the map with
     # dask.map, multiprocessing.map, ...
     all_solutions = map(partial(handle_request_single_vehicle, req=req), fleet_state)
     best_vehicle, min_cost,  new_stoplist = min(all_solutions, key=lambda x:x[1])
 
     if min_cost == inf: # no solution was found
         # TODO: return a RequestRejectedEvent
+        return RequestRejectionEvent()
     else:
         # TODO: modify the best vehicle's stoplist
+        fleet_state[best_vehicle] = new_stoplist
+        return RequestAcceptanceEvent()
 
 
-
-def simulate(initial_fleet_state: FleetState, requests: Iterator[Request]) -> List[Event]:
+def simulate(initial_fleet_state: FleetState, requests: Iterator[Request]) -> Iterator[Event]:
     # set internal clock to 0
     t = 0
     fleet_state = initial_fleet_state
@@ -51,8 +63,11 @@ def simulate(initial_fleet_state: FleetState, requests: Iterator[Request]) -> Li
                 if stop.estimated_arrival_time > req_epoch:
                     break
                 # TODO emit either a PickupEvent or a DeliveryEvent
+                # TODO optionally validate the traveltime velocity constraints
+
             # drop the visited stops
             stop_list = stop_list[:stop_idx]
+            # TODO: add the current position stop
 
         # handle the current request
-        handle_request(request, fleet_state)
+        yield handle_request(request, fleet_state)
