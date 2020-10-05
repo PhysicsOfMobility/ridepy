619942592417b5f275430c2684699d94d7890edf
Felix Jung <felix.jung@ds.mpg.de>
2020-10-05 17:26:52 +0200

    more infrastructure/tests

--- thesimulator/test/test_simulator.py
+++ thesimulator/test/test_simulator.py
@@ -12,7 +12,14 @@ def test_random_request_generator():
     assert all(
         reqs[i + 1].creation_timestamp > reqs[i].creation_timestamp for i in range(9)
     )
-    assert all(r.request_id is not None for r in reqs)
+    for r in reqs:
+        assert r.request_id is not None
+        assert len(r.origin) == 2
+        assert len(r.destination) == 2
+        assert 0 <= r.origin[0] <= 1
+        assert 0 <= r.origin[1] <= 1
+        assert 0 <= r.destination[0] <= 1
+        assert 0 <= r.destination[1] <= 1
 
 
 def test_fleetstate_simulate():
