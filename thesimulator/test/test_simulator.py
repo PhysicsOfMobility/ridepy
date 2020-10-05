ebc14936d7e4e4f3b4ea65433ece02077d1a27ea
Felix Jung <felix.jung@ds.mpg.de>
2020-10-05 17:25:00 +0200

    more infrastructure

--- thesimulator/test/test_simulator.py
+++ thesimulator/test/test_simulator.py
@@ -2,12 +2,17 @@ from thesimulator.fleet_state import FleetState
 from thesimulator.data_structures import TransportationRequest
 from thesimulator.utils import RandomRequestGenerator
 import itertools as it
+import operator as op
 
 
 def test_random_request_generator():
     rg = RandomRequestGenerator()
-    reqs = it.islice(rg, 10)
-    print(list(reqs))
+    reqs = list(it.islice(rg, 10))
+    assert len(reqs) == 10
+    assert all(
+        reqs[i + 1].creation_timestamp > reqs[i].creation_timestamp for i in range(9)
+    )
+    assert all(r.request_id is not None for r in reqs)
 
 
 def test_fleetstate_simulate():
