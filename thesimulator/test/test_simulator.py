550809f2c7fc07f3ddb05007b7aad3a00dc0bf0a
Felix Jung <felix.jung@ds.mpg.de>
2020-10-12 16:50:50 +0200

    temp intermediate

--- thesimulator/test/test_simulator.py
+++ thesimulator/test/test_simulator.py
@@ -5,12 +5,16 @@ import collections as cl
 import operator as op
 import numpy as np
 
+from tabulate import tabulate
+
 from thesimulator.fleet_state import SlowSimpleFleetState, MPIFuturesFleetState
 from thesimulator.data_structures import (
     Stop,
     InternalRequest,
     StopAction,
     PickupEvent,
+    StopEvent,
+    DeliveryEvent,
 )
 from thesimulator.util.request_generators import RandomRequestGenerator
 from thesimulator.util.spaces import Euclidean
@@ -69,6 +73,7 @@ def test_slow_simple_fleet_state_simulate(initial_stoplists):
     # print("\n".join(map(str, events)))
 
 
+@pytest.mark.n_buses(10)
 def test_mpi_futures_fleet_state_simulate(initial_stoplists):
     rg = RandomRequestGenerator(rate=10)
     reqs = list(it.islice(rg, 1000))
@@ -76,3 +81,52 @@ def test_mpi_futures_fleet_state_simulate(initial_stoplists):
     events = list(fs.simulate(reqs, t_cutoff=20))
     # print([event.vehicle_id for event in events if isinstance(event, PickupEvent)])
     # print("\n".join(map(str, events)))
+
+
+@pytest.mark.n_buses(10)
+def test_with_taxicab_dispatcher_simple_1(initial_stoplists):
+    rg = RandomRequestGenerator(rate=1)
+    reqs = list(it.islice(rg, 180))
+    fs = SlowSimpleFleetState(initial_stoplists=initial_stoplists, space=Euclidean())
+    events = list(fs.simulate(reqs))
+
+    stop_events = list(
+        filter(lambda x: isinstance(x, (PickupEvent, DeliveryEvent)), events)
+    )
+    vehicle_id_idxs = dict(
+        zip(set(map(op.attrgetter("vehicle_id"), stop_events)), it.count(1))
+    )
+
+    output_list = [
+        [None for _ in range(len(vehicle_id_idxs) + 1)] for _ in range(len(stop_events))
+    ]
+    max_r_c = int(max(x.request_id for x in stop_events) // 10)
+
+    for row, event in zip(output_list, stop_events):
+        row[0] = f"{event.timestamp:.2f}"
+        row[
+            vehicle_id_idxs[event.vehicle_id]
+        ] = f"{'pu' if isinstance(event, PickupEvent) else 'do'} {event.request_id}"  # f" {} {' '*int(max_r_c-event.request_id//10)}{event.request_id}"
+
+    print(tabulate(output_list, headers=["Name", "Age"], tablefmt="orgtbl"))
+
+    #
+    # max_r_c = int(max(x.request_id for x in stop_events) // 10)
+    # max_t_c = int(max(x.timestamp for x in stop_events) // 10)
+    #
+    # print(
+    #     "|"
+    #     + " " * (max_t_c + 1)
+    #     + "t    "
+    #     + "|"
+    #     + "|".join(" v " + " "*(n_vehicles//10)+f"{n}" for n in range(n_vehicles))
+    #     + "|"
+    # )
+    # for event in stop_events:
+    #     ostring = (
+    #         f"| {' '*int(max_t_c-event.timestamp//10)}{event.timestamp:.2f} |"
+    #         + (" " * (max_r_c + 6) + "|") * event.vehicle_id
+    #         + f" {'pu' if isinstance(event, PickupEvent) else 'do'} {' '*int(max_r_c-event.request_id//10)}{event.request_id} |"
+    #         + (" " * (max_r_c + 6) + "|") * (n_vehicles - event.vehicle_id - 1)
+    #     )
+    #     print(ostring)
