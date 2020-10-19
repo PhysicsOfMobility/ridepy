9931dad6a579ab02fbce5b8308db2677361e9fd9
Felix Jung <felix.jung@ds.mpg.de>
2020-10-19 12:34:56 +0200

    intermediate stuff, more spaces

--- thesimulator/util/spaces.py
+++ thesimulator/util/spaces.py
@@ -47,11 +47,7 @@ class Euclidean(TransportSpace):
             self.coord_range = [(0, 1)] * n_dimensions
 
     def d(self, u, v):
-        assert (
-            isinstance(u, (int, float))
-            and isinstance(v, (int, float))
-            or len(u) == len(v) == self.n_dimensions
-        ), "Dimensions of vectors must match"
+        assert len(u) == len(v) == self.n_dimensions, "Dimensions of vectors must match"
         return spd.euclidean(u, v)
 
     def t(self, u, v):
@@ -92,124 +88,51 @@ class Euclidean2D(Euclidean):
 
 
 class Graph(TransportSpace):
-    """
-    A location is identified with a one-dimensional coordinate, namely the node index.
-    """
-
-    def __init__(
-        self, graph: nx.Graph, distance_attribute="distance", velocity: float = 1
-    ):
-        self.G = graph
+    def __init__(self, G, distance_attribute, backend="networkx"):
+        self.G = G
         self.distance_attribute = distance_attribute
         (
             self._predecessors,
             self._distances,
         ) = nx.floyd_warshall_predecessor_and_distance(self.G, self.distance_attribute)
-        self.velocity = velocity
 
     @classmethod
-    def create_random(cls):
+    def create_random(cls, G):
         ...
 
     @classmethod
-    def create_grid(
-        cls,
-        dim=(3, 3),
-        periodic=False,
-        velocity: float = 1,
-        edge_distance=1,
-        distance_attribute="distance",
-    ):
-        graph = nx.grid_graph(dim=dim, periodic=periodic)
-        nx.set_edge_attributes(graph, edge_distance, distance_attribute)
-
-        return Graph(
-            graph=graph,
-            velocity=velocity,
-            distance_attribute=distance_attribute,
-        )
+    def create_grid(cls, G):
+        ...
 
     def d(self, u, v):
         return self._distances[u][v]
 
-    def t(self, u, v) -> Union[int, float]:
-        return self.d(u, v) / self.velocity
-
     def interp_dist(self, u, v, dist_to_dest):
-        """
+        # remaining time: go backward from destination vertex
 
-        Parameters
-        ----------
-        u
-        v
-        dist_to_dest
+        parent_distance = self.d(u, v)
 
-        Returns
-        -------
-        next_node
-        jump_distance
-
-        """
-
-        if u == v:
-            return v, 0
-
-        next_node = v
-        while next_node is not u:
-            predecessor = self._predecessors[u][next_node]
-            predecessor_dist = self.d(predecessor, v)
-            if predecessor_dist >= dist_to_dest:
+        w = v
+        while w is not u:
+            w = self._predecessors[u][w]
+            parent_distance = self.d(w, v)
+            if parent_distance >= dist_to_dest:
                 break
-            next_node = predecessor
 
-        if predecessor_dist > dist_to_dest:
-            return next_node, dist_to_dest - self.d(next_node, v)
+        if parent_distance > dist_to_dest:
+            # we are between parent vertex and v vertex
+            return w, dist_to_dest - self.d(w, v)
         else:
-            return predecessor, 0
-
-    def interp_time(self, u, v, time_to_dest):
-        """
-
-        Parameters
-        ----------
-        u
-        v
-        time_to_dest
-
-        Returns
-        -------
-        next_node
-        jump_time
-
-        """
-        next_node, jump_dist = self.interp_dist(
-            u, v, dist_to_dest=time_to_dest * self.velocity
-        )
-        return next_node, jump_dist / self.velocity
-
-    def shortest_path_vertex_sequence(self, u, v) -> List[ID]:
-        seq = [v]
-        if u != v:
-            next_node = v
-            while next_node is not u:
-                next_node = self._predecessors[u][next_node]
-                seq.append(next_node)
-            seq.append(u)
-        return seq[::-1]
-
-    def random_point(self):
-        return random.choice(list(self.G.nodes))
+            # we are at parent vertex
+            return w, 0.0
 
 
-class ContinuousGraph(Graph):
+class ContinuousGraph(TransportSpace):
     def d(self, u, v):
         """coordinates shall consist of triples (u, v, dist_to_dest)"""
         ...
 
     def t(self, u, v):
-        ...
-
-    def interp_dist(self, u, w, time_to_dest):
         ...
 
     def interp_time(self, u, v, time_to_dest):
