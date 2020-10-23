badbf125aa0b0ff20f39f2c12f8163a3d2f627dd
Felix Jung <felix.jung@ds.mpg.de>
2020-10-23 18:29:34 +0200

    euclidean fixed

--- test/test_space.py
+++ test/test_space.py
@@ -3,6 +3,17 @@ import math as m
 from thesimulator.util.spaces import Euclidean, Euclidean1D, Euclidean2D
 
 
+def test_Euclidean():
+    space = Euclidean(n_dimensions=1)
+    assert space.d(0, 1) == 1.0
+    assert space.d(0, 0) == 0.0
+
+    space = Euclidean(n_dimensions=2)
+    assert space.d((0, 0), (0, 1)) == 1.0
+    assert space.d((0, 0), (0, 0)) == 0.0
+    assert space.d((0, 0), (1, 1)) == m.sqrt(2)
+
+
 def test_Euclidean1D():
     space = Euclidean1D()
     assert space.d(0, 1) == 1.0
