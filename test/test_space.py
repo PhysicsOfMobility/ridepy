90d001b791a917be165c75bdc4d94cdcd4587f1b
Felix Jung <felix.jung@ds.mpg.de>
2020-10-23 19:51:43 +0200

    interpolation and tests

--- test/test_space.py
+++ test/test_space.py
@@ -1,6 +1,14 @@
 import pytest
 import math as m
-from thesimulator.util.spaces import Euclidean, Euclidean1D, Euclidean2D
+import numpy as np
+
+from thesimulator.util.spaces import (
+    Euclidean,
+    Euclidean1D,
+    Euclidean2D,
+    Graph,
+    ContinuousGraph,
+)
 
 
 def test_Euclidean():
@@ -25,3 +33,28 @@ def test_Euclidean2D():
     assert space.d((0, 0), (0, 1)) == 1.0
     assert space.d((0, 0), (0, 0)) == 0.0
     assert space.d((0, 0), (1, 1)) == m.sqrt(2)
+
+
+def test_Grid():
+    space = Graph.create_grid()
+    # breakpoint()
+    assert space.d((0, 0), (0, 0)) == 0
+    assert space.d((0, 0), (0, 1)) == 1
+    assert space.d((0, 1), (0, 2)) == 1
+    assert space.d((0, 0), (0, 2)) == 2
+    assert space.d((0, 0), (1, 2)) == 3
+    assert space.d((0, 0), (2, 2)) == 4
+    with pytest.raises(KeyError):
+        assert space.d((0, -1), (2, 2)) == 4
+
+    for dist_to_dest, (next_node, jump_time) in zip(
+        [2, 1.1, 1, 0.1, 0],
+        [((0, 0), 0), ((0, 1), 0.1), ((0, 1), 0), ((0, 2), 0.1), ((0, 2), 0)],
+    ):
+        next_node_interp, jump_time_interp = space.interp_dist(
+            (0, 0), (0, 2), dist_to_dest
+        )
+        assert next_node_interp == next_node
+        assert np.isclose(jump_time_interp, jump_time)
+
+    assert space.interp_dist((0, 0), (0, 0), 0) == ((0, 0), 0)
