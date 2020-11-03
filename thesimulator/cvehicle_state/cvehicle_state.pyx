bcef3617a34bca97bb67d99f71d09418034e6cc0
Debsankha Manik <dmanik@gwdg.de>
2020-11-03 19:46:27 +0100

    fix(cvehicle_state.pyx): converts python3 str to bytes before handding off to C++ constructor

--- thesimulator/cvehicle_state/cvehicle_state.pyx
+++ thesimulator/cvehicle_state/cvehicle_state.pyx
@@ -8,6 +8,6 @@ from libcpp.string cimport string
 cdef class Request:
     cdef CRequest crequest
 
-    def __cinit__(self, string request_id, float creation_timestamp):
-        self.crequest = CRequest(request_id, creation_timestamp)
+    def __cinit__(self, request_id, creation_timestamp):
+        self.crequest = CRequest(request_id.encode('ascii'), float(creation_timestamp))
 
