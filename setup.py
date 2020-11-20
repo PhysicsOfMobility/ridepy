19e5121360e91b585feaa013c1986019bcde808a
Debsankha Manik <dmanik@gwdg.de>
2020-11-20 22:40:03 +0100

    build: setup.py reads requirements files. gh actions no longer uses poetry

--- setup.py
+++ setup.py
@@ -2,5 +2,20 @@
 
 import setuptools
 
-if __name__ == "__main__":
-    setuptools.setup()
+with open('requirements.txt', 'r') as f:
+    reqs = f.readlines()
+
+with open('requirements-dev.txt', 'r') as f:
+    dev_reqs = f.readlines()
+
+setuptools.setup(
+    name='thesimulator',
+    version='0.1',
+    zip_safe = False,
+    packages=setuptools.find_packages(),
+    install_requires = reqs,
+    extras_require = {
+        'dev': dev_reqs
+        }
+)
+
