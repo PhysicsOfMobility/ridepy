e999ef581624c45695bed4084332e03606fce403
Felix Jung <felix.jung@tu-dresden.de>
2020-11-24 17:52:42 +0100

    fixed spaces (subtraction for dim>1)
    multiplication added

--- setup.py
+++ setup.py
@@ -2,20 +2,17 @@
 
 import setuptools
 
-with open('requirements.txt', 'r') as f:
+with open("requirements.txt", "r") as f:
     reqs = f.readlines()
 
-with open('requirements-dev.txt', 'r') as f:
+with open("requirements-dev.txt", "r") as f:
     dev_reqs = f.readlines()
 
 setuptools.setup(
-    name='thesimulator',
-    version='0.1',
-    zip_safe = False,
+    name="thesimulator",
+    version="0.1",
+    zip_safe=False,
     packages=setuptools.find_packages(),
-    install_requires = reqs,
-    extras_require = {
-        'dev': dev_reqs
-        }
+    install_requires=reqs,
+    extras_require={"dev": dev_reqs},
 )
-
