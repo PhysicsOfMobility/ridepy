8805d9e01a8604d9b07e4d8013caced164e11386
Debsankha Manik <dmanik@gwdg.de>
2020-11-29 23:10:32 +0100

    Cythonic vehicle_state started

--- thesimulator/cvehicle_state/cdata_structures.pyx
+++ thesimulator/cvehicle_state/cdata_structures.pyx
@@ -111,31 +111,6 @@ cdef class Stoplist:
         return stoplist
 
 
-@dataclass
-class TransportationRequest(Request):
-    """
-    A request for the system to perform a transportation task,
-    through creating a route through the system given spatio-temporal constraints.
-    """
-    origin: Any
-    destination: Any
-    # pickup_offset: float = 0
-    pickup_timewindow_min: Optional[float]
-    pickup_timewindow_max: Optional[float]
-    delivery_timewindow_min: Optional[float]
-    delivery_timewindow_max: Optional[float]
-
-
-@dataclass
-class InternalRequest(Request):
-    """
-    A request for the system to perform some action at a specific location
-    that is not directly requested by a customer
-    """
-
-    location: Any
-
-
 
 def spam():
     cdef CRequest r
@@ -148,7 +123,116 @@ def spam():
     return pyreq, pystop
 
 
-def dispatcher(Request request, Stoplist stoplist):
-#    cdef  = c_disp(request.c_req, stoplist.c_stoplist)
-#    return Stoplist.from_c(res)
-    pass
+class VehicleState:
+    """
+    Single vehicle insertion logic is implemented in Cython here.
+    """
+
+    def recompute_arrival_times_drive_first(self):
+        # update CPATs
+        for stop_i, stop_j in zip(self.stoplist, self.stoplist[1:]):
+            stop_j.estimated_arrival_time = max(
+                stop_i.estimated_arrival_time, stop_i.time_window_min
+            ) + self.space.t(stop_i.location, stop_j.location)
+
+    def __init__(
+        self, *, vehicle_id, initial_stoplist): # TODO currently transport_space cannot be specified
+        self.vehicle_id = vehicle_id
+        # TODO check for CPE existence in each supplied stoplist or encapsulate the whole thing
+        # Create a cython stoplist object from initial_stoplist
+        self.stoplist = Stoplist(initial_stoplist)
+        self.space = space
+
+    def fast_forward_time(self, t: float) -> List[StopEvent]:
+        """
+        Update the vehicle_state to the simulator time `t`.
+
+        Parameters
+        ----------
+        t
+            time to be updated to
+
+        Returns
+        -------
+        events
+            List of stop events emitted through servicing stops
+        """
+
+        # TODO assert that the CPATs are updated and the stops sorted accordingly
+        # TODO optionally validate the travel time velocity constraints
+
+        event_cache = []
+
+        last_stop = None
+
+        # drop all non-future stops from the stoplist, except for the (outdated) CPE
+        for i in range(len(self.stoplist) - 1, 0, -1):
+            stop = self.stoplist[i]
+            # service the stop at its estimated arrival time
+            if stop.estimated_arrival_time <= t:
+                # as we are iterating backwards, the first stop iterated over is the last one serviced
+                if last_stop is None:
+                    last_stop = stop
+
+                event_cache.append(
+                    {
+                        StopAction.pickup: PickupEvent,
+                        StopAction.dropoff: DeliveryEvent,
+                        StopAction.internal: InternalStopEvent,
+                    }[stop.action](
+                        request_id=stop.request.request_id,
+                        vehicle_id=self.vehicle_id,
+                        timestamp=max(
+                            stop.estimated_arrival_time, stop.time_window_min
+                        ),
+                    )
+                )
+
+                del self.stoplist[i]
+
+        # fix event cache order
+        event_cache = event_cache[::-1]
+
+        # if no stop was serviced, the last stop is the outdated CPE
+        if last_stop is None:
+            last_stop = self.stoplist[0]
+
+        # set CPE time to current time
+        self.stoplist[0].estimated_arrival_time = t
+
+        # set CPE location to current location as inferred from the time delta to the upcoming stop's CPAT
+        if len(self.stoplist) > 1:
+            self.stoplist[0].location, _ = self.space.interp_time(
+                u=last_stop.location,
+                v=self.stoplist[1].location,
+                time_to_dest=self.stoplist[1].estimated_arrival_time - t,
+            )
+        else:
+            # stoplist is empty, only CPE is there. Therefore we just stick around...
+            pass
+
+        return event_cache
+
+    def handle_transportation_request_single_vehicle(
+        self, request: TransportationRequest
+    ) -> SingleVehicleSolution:
+        """
+        The computational bottleneck. An efficient simulator could do the following:
+        1. Parallelize this over all vehicles. This function being without any side effects, it should be easy to do.
+        2. Implement as a c extension. The args and the return value are all basic c data types,
+           so this should also be easy.
+
+        Parameters
+        ----------
+        request
+
+        Returns
+        -------
+        This returns the single best solution for the respective vehicle.
+        """
+
+        return self.vehicle_id, *taxicab_dispatcher_drive_first(
+            request=request, stoplist=self.stoplist, space=self.space
+        )
+
+
