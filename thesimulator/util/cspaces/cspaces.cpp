0b0eb76a2c4bb2efb55c7dd55e3c4dc0dbf05f00
Debsankha Manik <debsankha.manik@moia.io>
2020-12-18 23:13:42 +0100

    fixed interpolation in manhattan

--- thesimulator/util/cspaces/cspaces.cpp
+++ thesimulator/util/cspaces/cspaces.cpp
@@ -50,29 +50,9 @@ double Manhattan2D::t(R2loc u, R2loc v) const
 
 pair<R2loc, double> Manhattan2D::interp_dist(R2loc u, R2loc v, double dist_to_dest) const
 {
-    double dist = this->t(u, v);
-    double sofar = dist - dist_to_dest;
-    double dx = v.first - u.first;
-    double dy = v.second - u.second;
-
-    double x,y;
-
-    if (sofar < std::abs(dx))
-    {
-        y = u.second;
-        if (dx >0) x = u.first+sofar;
-        else  x = u.first-sofar;
-    }
-    else
-    {
-        x = v.first;
-        sofar -= std::abs(v.first-x);
-        if (dy>0) y = u.second+sofar;
-        else y = u.second-sofar;
-    }
-
+    double frac = dist_to_dest/this->d(u, v);
     return make_pair(
-        R2loc {x,y}, 0);
+        R2loc {u.first*frac + (1-frac)*v.first, u.second*frac + (1-frac)*v.second}, 0);
 }
 
 pair<R2loc, double> Manhattan2D::interp_time(R2loc u, R2loc v, double time_to_dest) const
