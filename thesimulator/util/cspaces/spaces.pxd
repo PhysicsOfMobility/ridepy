5373efc705b11b65b5a62ebf9134f5727734f986
Debsankha Manik <debsankha.manik@moia.io>
2021-01-09 14:22:32 +0100

    Unionized cython spaces compiles, dunno if works

--- thesimulator/util/cspaces/spaces.pxd
+++ thesimulator/util/cspaces/spaces.pxd
@@ -1,23 +1,31 @@
 # distutils: language = c++
 
 from .cspaces cimport (
-Euclidean2D as CEuclidean2D,
-TransportSpace as CTransportSpace,
-TransportSpaceR2loc as CTransportSpaceR2loc,
-TransportSpaceInt as CTransportSpaceInt,
-Manhattan2D as CManhattan2D
+    Euclidean2D as CEuclidean2D,
+    TransportSpace as CTransportSpace,
+    Manhattan2D as CManhattan2D
 )
 
+from thesimulator.cdata_structures.data_structures cimport LocType, R2loc
 
 #cdef class TransportSpace:
 #    cdef CTransportSpace *c_space_ptr
 
-cdef class TransportSpaceR2loc:
-    cdef CTransportSpaceR2loc *c_space_ptr
+cdef union USpace:
+    CTransportSpace[R2loc] *space_r2loc_ptr
+    CTransportSpace[int] *space_int_ptr
 
-cdef class Euclidean2D(TransportSpaceR2loc):
+
+#cdef class TransportSpaceR2loc:
+#    cdef CTransportSpaceR2loc *c_space_ptr
+
+cdef class TransportSpace:
+    cdef USpace u_space
+    cdef LocType loc_type
+
+cdef class Euclidean2D(TransportSpace):
     cdef CEuclidean2D *derived_ptr
 
-cdef class Manhattan2D(TransportSpaceR2loc):
+cdef class Manhattan2D(TransportSpace):
     cdef CManhattan2D *derived_ptr
 
--- thesimulator/util/cspaces/spaces.pyx
+++ thesimulator/util/cspaces/spaces.pyx
@@ -4,6 +4,9 @@ from .cspaces cimport(
     Euclidean2D as CEuclidean2D,
     Manhattan2D as CManhattan2D,
 )
+
+from thesimulator.cdata_structures.data_structures cimport LocType, R2loc
+
 from cython.operator cimport dereference
 
 """
@@ -40,37 +43,87 @@ in great length at https://stackoverflow.com/a/28727488. Basically:
 #    def interp_time(self, R2loc u, R2loc v, double time_to_dest):
 #        return dereference(self.c_space_ptr).interp_time(u, v, time_to_dest)
 
-cdef class TransportSpaceR2loc:
-    """
-    We do not need to allocate/free self.c_space_ptr at all since This is a wrapper around the c++ abstract class
-    and will never be instantiated.
-    """
-    # TODO: Need to template away to eliminate hard requirement that locations are R2loc.
-    # However, does anything need to be done at all apart from removing all the type declarations from the
-    # method arguments?
-    def d(self, R2loc u, R2loc v):
-        return dereference(self.c_space_ptr).d(u, v)
 
-    def t(self, R2loc u, R2loc v):
-        return dereference(self.c_space_ptr).t(u, v)
+#cdef class TransportSpaceR2loc:
+#    """
+#    We do not need to allocate/free self.c_space_ptr at all since This is a wrapper around the c++ abstract class
+#    and will never be instantiated.
+#    """
+#    # TODO: Need to template away to eliminate hard requirement that locations are R2loc.
+#    # However, does anything need to be done at all apart from removing all the type declarations from the
+#    # method arguments?
+#    def d(self, R2loc u, R2loc v):
+#        return dereference(self.c_space_ptr).d(u, v)
+#
+#    def t(self, R2loc u, R2loc v):
+#        return dereference(self.c_space_ptr).t(u, v)
+#
+#    def interp_dist(self, R2loc u, R2loc v, double dist_to_dest):
+#        return dereference(self.c_space_ptr).interp_dist(u, v, dist_to_dest)
+#
+#    def interp_time(self, R2loc u, R2loc v, double time_to_dest):
+#        return dereference(self.c_space_ptr).interp_time(u, v, time_to_dest)
+#
 
-    def interp_dist(self, R2loc u, R2loc v, double dist_to_dest):
-        return dereference(self.c_space_ptr).interp_dist(u, v, dist_to_dest)
 
-    def interp_time(self, R2loc u, R2loc v, double time_to_dest):
-        return dereference(self.c_space_ptr).interp_time(u, v, time_to_dest)
 
-cdef class Euclidean2D(TransportSpaceR2loc):
+cdef class TransportSpace:
+    def __init__(self, loc_type):
+        if loc_type == 'int':
+            self.loc_type = LocType.INT
+        elif loc_type == 'r2loc':
+            self.loc_type = LocType.R2LOC
+        else:
+            raise ValueError("This line should never have been reached")
+
+    def d(self, u, v):
+        if self.loc_type == LocType.R2LOC:
+            return dereference(self.u_space.space_r2loc_ptr).d(<R2loc>u, <R2loc>v)
+        elif self.loc_type == LocType.INT:
+            return dereference(self.u_space.space_int_ptr).d(<int>u, <int>v)
+        else:
+            raise ValueError("This line should never have been reached")
+
+    def t(self, u, v):
+        if self.loc_type == LocType.R2LOC:
+            return dereference(self.u_space.space_r2loc_ptr).t(<R2loc>u, <R2loc>v)
+        elif self.loc_type == LocType.INT:
+            return dereference(self.u_space.space_int_ptr).t(<int>u, <int>v)
+
+    def interp_dist(self, u, v, double dist_to_dest):
+        if self.loc_type == LocType.R2LOC:
+            return dereference(self.u_space.space_r2loc_ptr).interp_dist(<R2loc>u, <R2loc>v, dist_to_dest)
+        elif self.loc_type == LocType.INT:
+            return dereference(self.u_space.space_int_ptr).interp_dist(<int>u, <int>v, dist_to_dest)
+        else:
+            raise ValueError("This line should never have been reached")
+
+    def interp_time(self, u, v, double time_to_dest):
+        if self.loc_type == LocType.R2LOC:
+            return dereference(self.u_space.space_r2loc_ptr).interp_time(<R2loc>u, <R2loc>v, time_to_dest)
+        elif self.loc_type == LocType.INT:
+            return dereference(self.u_space.space_int_ptr).interp_time(<int>u, <int>v, time_to_dest)
+        else:
+            raise ValueError("This line should never have been reached")
+
+    def __dealloc__(self):
+        #free the correct pointer
+        ...
+
+
+cdef class Euclidean2D(TransportSpace):
     def __cinit__(self, double velocity=1):
-        self.derived_ptr = self.c_space_ptr = new  CEuclidean2D(velocity)
+        self.loc_type == LocType.R2LOC
+        self.derived_ptr = self.u_space.space_r2loc_ptr = new CEuclidean2D(velocity)
 
     def __dealloc__(self):
         del self.derived_ptr
 
 
-cdef class Manhattan2D(TransportSpaceR2loc):
+cdef class Manhattan2D(TransportSpace):
     def __cinit__(self, double velocity=1):
-        self.derived_ptr = self.c_space_ptr = new  CManhattan2D(velocity)
+        self.loc_type == LocType.R2LOC
+        self.derived_ptr = self.u_space.space_r2loc_ptr = new CManhattan2D(velocity)
 
     def __dealloc__(self):
         del self.derived_ptr
