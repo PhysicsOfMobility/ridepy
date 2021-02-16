d5bb1a75594fc87191a1dd0aa3e0161aadcbe5d1
Felix Jung <felix.jung@tu-dresden.de>
2021-02-16 12:06:27 +0100

    magic decorator

--- thesimulator/util/spaces.py
+++ thesimulator/util/spaces.py
@@ -193,10 +193,7 @@ class Graph(TransportSpace):
 
     @smartVectorize
     def d(self, u, v):
-        if isinstance(u, pd.Series) and isinstance(v, pd.Series):
-            return pd.DataFrame((u, v)).apply(lambda r: self._distances[r[0]][r[1]])
-        else:
-            return self._distances[u][v]
+        return self._distances[u][v]
 
     def t(self, u, v) -> Union[int, float]:
         return self.d(u, v) / self.velocity
