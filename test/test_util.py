caa3bc949ee9349d8da36bfb00bb816474e89729
Felix Jung <felix.jung@tu-dresden.de>
2021-02-17 11:57:57 +0100

    tests finished

--- test/test_util.py
+++ test/test_util.py
@@ -50,34 +50,52 @@ def test_smartVectorize():
     vs = pd.Series(va)
     es = pd.Series(ea)
 
-    assert phony1.foo(u, v) == e
-    assert phony2.foo(u, v) == e
-    assert phony3.foo(u, v) == e
+    for w1, w2, w3 in [
+        [
+            lambda u, v: phony1.foo(u, v),
+            lambda u, v: phony2.foo(u, v),
+            lambda u, v: phony3.foo(u, v),
+        ],
+        [
+            lambda u, v: phony1.foo(u, v=v),
+            lambda u, v: phony2.foo(u, v=v),
+            lambda u, v: phony3.foo(u, v=v),
+        ],
+        [
+            lambda u, v: phony1.foo(u=u, v=v),
+            lambda u, v: phony2.foo(u=u, v=v),
+            lambda u, v: phony3.foo(u=u, v=v),
+        ],
+    ]:
 
-    with pytest.raises(TypeError, match=r"numpy.*define __round__"):
-        phony1.foo(ua, va)
-    assert np.array_equal(phony2.foo(ua, va), ea)
-    assert np.array_equal(phony3.foo(ua, va), ea + 1)
+        assert w1(u, v) == e
+        assert w2(u, v) == e
+        assert w3(u, v) == e
 
-    with pytest.raises(TypeError, match=r"list.*define __round__"):
-        phony1.foo(ul, vl)
-    assert np.array_equal(phony2.foo(ul, vl), el)
-    assert np.array_equal(phony3.foo(ul, vl), el + 1)
+        with pytest.raises(TypeError, match=r"numpy.*define __round__"):
+            w1(ua, va)
+        assert np.array_equal(w2(ua, va), ea)
+        assert np.array_equal(w3(ua, va), ea + 1)
 
-    assert np.array_equal(phony1.foo(us, vs), es)
-    assert np.array_equal(phony2.foo(us, vs), es)
-    assert np.array_equal(phony3.foo(us, vs), es + 1)
+        with pytest.raises(TypeError, match=r"list.*define __round__"):
+            w1(ul, vl)
+        assert np.array_equal(w2(ul, vl), el)
+        assert np.array_equal(w3(ul, vl), el + 1)
 
-    x = np.zeros((100, 2))
+        assert np.array_equal(w1(us, vs), es)
+        assert np.array_equal(w2(us, vs), es)
+        assert np.array_equal(w3(us, vs), es + 1)
 
-    with pytest.raises(ValueError, match=r"2-dimension.*expected 1 dim"):
-        phony2.foo(x, x)
-    with pytest.raises(ValueError, match=r"2-dimension.*expected 1 dim"):
-        phony3.foo(x, x)
+        x = np.zeros((100, 2))
 
-    y = np.zeros(100)
-    z = np.zeros(50)
-    with pytest.raises(ValueError, match=r"shapes must match"):
-        phony2.foo(y, z)
-    with pytest.raises(ValueError, match=r"shapes must match"):
-        phony3.foo(y, z)
+        with pytest.raises(ValueError, match=r"2-dimension.*expected 1 dim"):
+            w2(x, x)
+        with pytest.raises(ValueError, match=r"2-dimension.*expected 1 dim"):
+            w3(x, x)
+
+        y = np.zeros(100)
+        z = np.zeros(50)
+        with pytest.raises(ValueError, match=r"shapes must match"):
+            w2(y, z)
+        with pytest.raises(ValueError, match=r"shapes must match"):
+            w3(y, z)
