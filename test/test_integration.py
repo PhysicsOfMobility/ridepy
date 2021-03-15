0970a8c0582a537329c9f1fab2745df394f3e8a6
Felix Jung <felix.jung@tu-dresden.de>
2021-03-15 20:11:34 +0100

    TODO added

--- test/test_integration.py
+++ test/test_integration.py
@@ -26,10 +26,10 @@ def test_notebooks(tmp_path):
         assert tmp_py_path.exists()
         shutil.copy(tmp_py_path, tmp_ipy_path)
         assert tmp_ipy_path.exists()
+        # TODO: substitute this with something along these lines
+        # IPython.get_ipython().safe_execfile(fname, shell_futures=False, raise_exceptions=True)
         res = subprocess.run(["ipython", str(tmp_ipy_path)], capture_output=True)
         stdout = res.stdout.decode()
         stderr = res.stderr.decode()
         assert not "WARNING" in stderr
         assert not "Error" in stdout
-
-    # IPython.get_ipython().safe_execfile(fname, shell_futures=False, raise_exceptions=True)
