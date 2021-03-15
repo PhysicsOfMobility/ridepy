04e8d4eab60a075974fe2d4626eeba79cfe2e05b
Felix Jung <felix.jung@tu-dresden.de>
2021-03-15 20:24:06 +0100

    added doc

--- test/test_integration.py
+++ test/test_integration.py
@@ -17,6 +17,18 @@ def test_notebooks(tmp_path):
 
     for fpath in Path("notebooks").glob("*.py"):
         print(fpath)
+
+        # This stunt is necessary to deal with magic commands
+        # Constraints:
+        # - Magics must not be in the py light script files, because black will complain.
+        #   Also jupytext doesn't do this on default. This can be changed by modifying the
+        #   notebooks' metadata, but this is obviously not practical to do for every new notebook.
+        # - Magics must be there for the test to work, as e.g. `%time result = my_expensive_computation()`
+        #   will leave `result` undefined if the line is commented by jupytext
+        # - ipython will understand magics, but only accept them in files ending with `.ipy`
+        # - jupytext will not store anything to files ending with `.ipy`
+        # - we only want to keep `.py` files in the notebooks directory (apart from `.ipynb` files, locally)
+        # this leaves us with the this garbage.
         jupytext.write(
             jupytext.read(fpath, fmt="py"),
             tmp_py_path,
@@ -26,9 +38,11 @@ def test_notebooks(tmp_path):
         assert tmp_py_path.exists()
         shutil.copy(tmp_py_path, tmp_ipy_path)
         assert tmp_ipy_path.exists()
+
         # TODO: substitute this with something along these lines
         # IPython.get_ipython().safe_execfile(fname, shell_futures=False, raise_exceptions=True)
         res = subprocess.run(["ipython", str(tmp_ipy_path)], capture_output=True)
+
         stdout = res.stdout.decode()
         stderr = res.stderr.decode()
         assert not "WARNING" in stderr
