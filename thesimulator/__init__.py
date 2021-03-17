721134c96bb829a15cd6891f642036720e6fa3c0
Felix Jung <fxjung@users.noreply.github.com>
2021-03-17 14:13:30 +0100

    reduced default loglevel to warning (#99)

--- thesimulator/__init__.py
+++ thesimulator/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.WARNING)
 handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
+handler.setLevel(logging.WARNING)
 logger.addHandler(handler)
