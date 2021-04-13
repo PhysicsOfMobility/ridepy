9af9b249d8f4ce7c526128926095254ff886ac44
Felix Jung <fxjung@users.noreply.github.com>
2021-04-13 21:06:45 +0200

    Polish notebooks (#140)
    * intro and intro cy now produce identical output
    
    * polished graph example notebook

--- notebooks/introduction_cython.py
+++ notebooks/introduction_cython.py
@@ -7,28 +7,39 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.6.0
+#       jupytext_version: 1.11.0
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
 
-# +
+# + tags=[]
 # %matplotlib inline
 
-import dataclasses
-
 import itertools as it
-import functools as ft
-import operator as op
 import math as m
 import numpy as np
 import pandas as pd
-import networkx as nx
 import matplotlib.pyplot as plt
 
-# +
+# + tags=[]
+from thesimulator.fleet_state import SlowSimpleFleetState
+from thesimulator.vehicle_state_cython import VehicleState
+
+from thesimulator.util.dispatchers_cython import (
+    brute_force_total_traveltime_minimizing_dispatcher,
+)
+
+from thesimulator.util.request_generators import RandomRequestGenerator
+from thesimulator.util.spaces_cython import Euclidean2D
+from thesimulator.data_structures_cython import TransportationRequest
+
+from thesimulator.util.analytics import get_stops_and_requests
+from thesimulator.util.analytics.plotting import plot_occupancy_hist
+
+# + tags=[]
+# assume dark background for plots?
 dark = True
 
 if dark:
@@ -38,26 +49,7 @@ if dark:
     plt.rcParams["axes.facecolor"] = (1, 1, 1, 0)
     plt.rcParams["figure.facecolor"] = (1, 1, 1, 0)
 
-# -
-
-from thesimulator.fleet_state import SlowSimpleFleetState, MPIFuturesFleetState
-from thesimulator.vehicle_state_cython import VehicleState
-from thesimulator.data_structures_cython import (
-    Stop,
-    InternalRequest,
-    StopAction,
-    TransportationRequest,
-)
-from thesimulator.util.dispatchers_cython import (
-    brute_force_total_traveltime_minimizing_dispatcher,
-)
-from thesimulator.util.request_generators import RandomRequestGenerator
-from thesimulator.util.spaces import Euclidean2D as pyEuclidean2D
-from thesimulator.util.spaces_cython import Euclidean2D
-from thesimulator.util.analytics import get_stops_and_requests
-from thesimulator.util.analytics.plotting import plot_occupancy_hist
-
-# +
+# + tags=[]
 pd.set_option("display.max_rows", 500)
 pd.set_option("display.max_columns", 500)
 pd.set_option("display.width", 1000)
@@ -65,22 +57,14 @@ pd.set_option("display.width", 1000)
 evf = lambda S, f, **arg: (S, f(S, **arg))
 # -
 
-# # configure the simulation and supply initial values
+# # Configure the simulation and supply initial values
 
-# +
+# + tags=[]
 n_buses = 50
-"""number of vehicles to simulate"""
 
 initial_location = (0, 0)
 
-# -
-
-
-# ## define simulation environment
-
-# +
 space = Euclidean2D()
-"""transport space"""
 
 rg = RandomRequestGenerator(
     rate=10,
@@ -88,50 +72,65 @@ rg = RandomRequestGenerator(
     max_delivery_delay_rel=1.9,
     space=space,
     request_class=TransportationRequest,
+    seed=42,
 )
-"""request generator"""
 
-# generate 100 random requests
+# create iterator yielding 100 random requests
 transportation_requests = it.islice(rg, 100)
+# -
 
-# initialize the simulator
+
+# ## Initialize a `FleetState`
+
+# + tags=[]
 fs = SlowSimpleFleetState(
     initial_locations={vehicle_id: initial_location for vehicle_id in range(n_buses)},
+    seat_capacities=8,
     space=space,
     dispatcher=brute_force_total_traveltime_minimizing_dispatcher,
-    seat_capacities=8,
     vehicle_state_class=VehicleState,
 )
 # -
 
-# ## perform the simulation
+# ## Perform the simulation
 
+# + tags=[]
 # exhaust the simulator's iterator
 # %time events = list(fs.simulate(transportation_requests))
+# -
 
-# ## process the results
+# ## Process the results
 
 
-stops, reqs = get_stops_and_requests(events=events, space=pyEuclidean2D())
+# + tags=[]
+stops, reqs = get_stops_and_requests(events=events, space=Euclidean2D())
+# -
 
-# # some distributions
-# ## relative travel times
+# # Some distributions
+# ## Relative travel times
 
+# + tags=[]
 reqs[("inferred", "relative_travel_time")].hist(bins=np.r_[1:5:20j])
 plt.gca().set_yscale("log")
+# -
 
 
-# ## waiting times
+# ## Waiting times
 
+# + tags=[]
 reqs[("inferred", "waiting_time")].hist(bins=np.r_[1:3:20j])
-# plt.gca().set_yscale('log')
+# -
 
 
-# ## direct travel times
+# ## Direct travel times
 
-reqs[("submitted", "direct_travel_time")].hist(bins=np.r_[0:1.5:30j])
+# + tags=[]
+reqs[("submitted", "direct_travel_time")].hist(bins=np.r_[0 : m.sqrt(2) : 30j])
+# -
 
 
-# ## occupancies
+# ## Occupancies
 
+# + tags=[]
 plot_occupancy_hist(stops)
+# -
