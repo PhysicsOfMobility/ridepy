7161f89107748271628be5e6dbfa91b97d46fedb
Felix Jung <felix.jung@tu-dresden.de>
2021-04-26 20:00:01 +0200

    SimulationSet now calling simulate_parameter_combinations

--- thesimulator/extras/simulation_set.py
+++ thesimulator/extras/simulation_set.py
@@ -151,6 +151,62 @@ def perform_single_simulation(
     return sim_id
 
 
+def simulate_parameter_combinations(
+    *,
+    param_combinations: Iterator[dict[str, dict[str, Sequence[Any]]]],
+    data_dir: Union[str, Path],
+    debug: bool = False,
+    max_workers: Optional[int] = None,
+    process_chunksize: int = 1,
+    jsonl_chunksize: int = 1000,
+    result_path_suffix: str = ".jsonl",
+    param_path_suffix: str = "_params.json",
+):
+    """
+    Run simulations for different parameter combinations using multiprocessing.
+
+    Parameters
+    ----------
+    param_combinations
+        An iterable of parameter configurations. For more detail see :ref:`Executing Simulations`
+    data_dir
+        Directory in which to store the parameters and results.
+    debug
+        Print debug info.
+    max_workers
+        Maximum number of multiprocessing workers. Defaults to number of processors
+        on the machine if `None` or not given.
+    process_chunksize
+        Number of simulations to submit to each multiprocessing worker at a time.
+    jsonl_chunksize
+        Maximum number of events to keep in memory before saving to disk
+    param_path_suffix
+        Parameters will be stored under "data_dir/<simulation_id><param_path_suffix>"
+    result_path_suffix
+        Simulation results will be stored under "data_dir/<simulation_id><result_path_suffix>"
+
+    Returns
+    -------
+        List of simulation IDs. See the docstring of `.SimulationSet` for more detail.
+    """
+    with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
+        sim_ids = list(
+            executor.map(
+                ft.partial(
+                    perform_single_simulation,
+                    debug=debug,
+                    jsonl_chunksize=jsonl_chunksize,
+                    data_dir=data_dir,
+                    param_path_suffix=param_path_suffix,
+                    result_path_suffix=result_path_suffix,
+                ),
+                param_combinations,
+                chunksize=process_chunksize,
+            )
+        )
+    return sim_ids
+
+
 class SimulationSet:
     """
     A set of simulations. The parameter space is defined through constant `base_params`,
@@ -489,23 +545,16 @@ class SimulationSet:
             - by result file `SimulationSet.result_paths`
         """
 
-        with concurrent.futures.ProcessPoolExecutor(
-            max_workers=self.max_workers
-        ) as executor:
-            self._result_ids = list(
-                executor.map(
-                    ft.partial(
-                        perform_single_simulation,
-                        debug=self.debug,
-                        jsonl_chunksize=self.jsonl_chunksize,
-                        data_dir=self.data_dir,
-                        param_path_suffix=self._param_path_suffix,
-                        result_path_suffix=self._result_path_suffix,
-                    ),
-                    iter(self),
-                    chunksize=self.process_chunksize,
-                )
-            )
+        self._result_ids = simulate_parameter_combinations(
+            param_combinations=iter(self),
+            data_dir=self.data_dir,
+            debug=self.debug,
+            max_workers=self.max_workers,
+            process_chunksize=self.process_chunksize,
+            jsonl_chunksize=self.jsonl_chunksize,
+            result_path_suffix=self._result_path_suffix,
+            param_path_suffix=self._param_path_suffix,
+        )
 
     def __len__(self) -> int:
         """
@@ -524,59 +573,3 @@ class SimulationSet:
             ),
         )
         return zip_part * product_part
-
-
-def simulate_parameter_combinations(
-    *,
-    param_combinations: Iterator[dict[str, dict[str, Sequence[Any]]]],
-    data_dir: Union[str, Path],
-    debug: bool = False,
-    max_workers: Optional[int] = None,
-    process_chunksize: int = 1,
-    jsonl_chunksize: int = 1000,
-    result_path_suffix: str = ".jsonl",
-    param_path_suffix: str = "_params.json",
-):
-    """
-    Run simulations for different parameter combinations using multiprocessing.
-
-    Parameters
-    ----------
-    param_combinations
-        An iterable of parameter configurations. For more detail see :ref:`Executing Simulations`
-    data_dir
-        Directory in which to store the parameters and results.
-    debug
-        Print debug info.
-    max_workers
-        Maximum number of multiprocessing workers. Defaults to number of processors
-        on the machine if `None` or not given.
-    process_chunksize
-        Number of simulations to submit to each multiprocessing worker at a time.
-    jsonl_chunksize
-        Maximum number of events to keep in memory before saving to disk
-    param_path_suffix
-        Parameters will be stored under "data_dir/<simulation_id><param_path_suffix>"
-    result_path_suffix
-        Simulation results will be stored under "data_dir/<simulation_id><result_path_suffix>"
-
-    Returns
-    -------
-        List of simulation IDs. See the docstring of `.SimulationSet` for more detail.
-    """
-    with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
-        sim_ids = list(
-            executor.map(
-                ft.partial(
-                    perform_single_simulation,
-                    debug=debug,
-                    jsonl_chunksize=jsonl_chunksize,
-                    data_dir=data_dir,
-                    param_path_suffix=param_path_suffix,
-                    result_path_suffix=result_path_suffix,
-                ),
-                param_combinations,
-                chunksize=process_chunksize,
-            )
-        )
-    return sim_ids
