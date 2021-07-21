af68a33ad4643e7cb968fe557fe0bb80a55dc585
Felix Jung <felix.jung@tu-dresden.de>
2021-07-21 12:06:06 +0200

    intermediate refactor commit

--- ridepy/util/analytics/__init__.py
+++ ridepy/util/analytics/__init__.py
@@ -12,6 +12,7 @@ from ridepy.data_structures import (
     TransportSpace,
 )
 from ridepy.events import VehicleStateEndEvent, VehicleStateBeginEvent
+from ridepy.util.analytics.helpers_cython import stop_properties
 
 
 def _create_events_dataframe(events: Iterable[dict]) -> pd.DataFrame:
@@ -486,183 +487,25 @@ def _add_insertion_stats_to_stoplist_dataframe(*, reqs, stops, space) -> pd.Data
     )
     actual_stops = stops.dropna(subset=("timestamp_submitted",))
 
-    def _properties_at_time(stop, full_sl, scope):
-        lo = stop["location"]
+    foo = stop_properties(stops)
 
-        stop = stop[
-            [
-                "timestamp",
-                "delta_occupancy",
-                "request_id",
-                "state_duration",
-                "occupancy",
-                "dist_to_next",
-                "time_to_next",
-                "timestamp_submitted",
-            ]
-        ].to_numpy(dtype="f8")
-
-        locations = np.array(full_sl["location"].to_list())
-
-        full_sl = full_sl[
-            [
-                "timestamp",
-                "delta_occupancy",
-                "request_id",
-                "state_duration",
-                "occupancy",
-                "dist_to_next",
-                "time_to_next",
-                "timestamp_submitted",
-            ]
-        ].to_numpy(dtype="f8")
-
-        [
-            TIMESTAMP,
-            DELTA_OCCUPANCY,
-            REQUEST_ID,
-            STATE_DURATION,
-            OCCUPANCY,
-            DIST_TO_NEXT,
-            TIME_TO_NEXT,
-            TIMESTAMP_SUBMITTED,
-        ] = range(8)
-
-        t = stop[TIMESTAMP]
-        ts = stop[TIMESTAMP_SUBMITTED]
-        pu = True if stop[DELTA_OCCUPANCY] > 0 else False
-        rid = stop[REQUEST_ID]
-
-        get_i_pu = lambda _sl, rid: np.argmax(_sl[:, REQUEST_ID] == rid)
-        get_i_do = (
-            lambda _sl, rid: len(_sl) - np.argmax((_sl[:, REQUEST_ID] == rid)[::-1]) - 1
-        )
-
-        mask = (full_sl[:, TIMESTAMP_SUBMITTED] <= t) & (t <= full_sl[:, TIMESTAMP])
-        sl = full_sl[mask]
-        loc = locations[mask]
-
-        mask_s = (full_sl[:, TIMESTAMP_SUBMITTED] <= ts) & (ts <= full_sl[:, TIMESTAMP])
-        sl_s = full_sl[mask_s]
-        loc_s = locations[mask_s]
-
-        if pu:
-            i_pu_sl = get_i_pu(sl, rid)
-            i_do_sl = get_i_do(sl, rid)
-            i_pu_sl_s = get_i_pu(sl_s, rid)
-            i_do_sl_s = get_i_do(sl_s, rid)
-
-            i_stop_sl = i_pu_sl
-            i_stop_sl_s = i_pu_sl_s
-        else:
-            i_do_sl = get_i_do(sl, rid)
-
-            i_pu_sl_s = get_i_pu(sl_s, rid)
-            i_do_sl_s = get_i_do(sl_s, rid)
-
-            i_stop_sl = i_do_sl
-            i_stop_sl_s = i_do_sl_s
-
-        res = {}
-
-        if scope != "system":
-            res["insertion_index"] = len(sl_s[sl_s[:, TIMESTAMP] < t])
-
-            def _get_legs(i_stop, _loc, time_desc):
-                l_sl = len(_loc)
-                if i_stop == 0 == l_sl - 1:
-                    stop_leg_1 = 0
-                    stop_leg_2 = 0
-                    stop_leg_d = 0
-                elif i_stop == 0:
-                    stop_leg_1 = 0
-                    stop_leg_2 = space.d(lo, _loc[i_stop + 1])
-                    stop_leg_d = stop_leg_1
-                elif i_stop == l_sl - 1:
-                    stop_leg_1 = space.d(_loc[i_stop - 1], lo)
-                    stop_leg_2 = 0
-                    stop_leg_d = stop_leg_2
-                else:
-                    stop_leg_1 = space.d(_loc[i_stop - 1], lo)
-                    stop_leg_2 = space.d(lo, _loc[i_stop + 1])
-                    stop_leg_d = space.d(
-                        _loc[i_stop - 1],
-                        _loc[i_stop + 1],
-                    )
-
-                stop_detour = stop_leg_1 + stop_leg_2 - stop_leg_d
-
-                return {
-                    f"leg_1_dist_{time_desc}_time": stop_leg_1,
-                    f"leg_2_dist_{time_desc}_time": stop_leg_2,
-                    f"leg_direct_dist_{time_desc}_time": stop_leg_d,
-                    f"detour_dist_{time_desc}_time": stop_detour,
-                }
-
-            res |= _get_legs(i_stop_sl, loc, "service")
-            res |= _get_legs(i_stop_sl_s, loc_s, "submission")
-
-        if pu:
-            sl = np.delete(sl, [i_pu_sl, i_do_sl], axis=0)
-        else:
-            sl = np.delete(sl, i_do_sl, axis=0)
-
-        sl_s = np.delete(sl_s, [i_pu_sl_s, i_do_sl_s], axis=0)
-
-        res[
-            f"{'system_' if scope=='system' else ''}stoplist_length_submission_time"
-        ] = len(sl_s)
-        res[
-            f"{'system_' if scope=='system' else ''}stoplist_length_service_time"
-        ] = len(sl)
-
-        res[
-            f"avg_{'system_' if scope=='system' else ''}segment_dist_submission_time"
-        ] = np.mean(sl_s[:, DIST_TO_NEXT])
-        res[
-            f"avg_{'system_' if scope=='system' else ''}segment_time_submission_time"
-        ] = np.mean(sl_s[:, TIME_TO_NEXT])
-
-        res[
-            f"avg_{'system_' if scope=='system' else ''}segment_dist_service_time"
-        ] = np.mean(sl[:, DIST_TO_NEXT])
-        res[
-            f"avg_{'system_' if scope=='system' else ''}segment_time_service_time"
-        ] = np.mean(sl[:, TIME_TO_NEXT])
-
-        return res
-
-    # def _properties_at_time(stop, full_sl, scope):
-    #     return Cython.inline(
-    #         """
-    #         print(scope)
+    # stops = stops.merge(
+    #     actual_stops.groupby("vehicle_id").apply(ft.partial(stop_properties, scope='vehicle', space=space)),
+    #     left_index=True,
+    #     right_index=True,
+    #     how="left",
+    # )
     #
-    #         """
-    #     )
-
-    stops = stops.merge(
-        actual_stops.groupby("vehicle_id").apply(
-            lambda df: df.apply(
-                ft.partial(_properties_at_time, full_sl=df, scope="vehicle"),
-                axis=1,
-                result_type="expand",
-            )
-        ),
-        left_index=True,
-        right_index=True,
-        how="left",
-    )
-
-    stops = stops.merge(
-        actual_stops.apply(
-            ft.partial(_properties_at_time, full_sl=stops, scope="system"),
-            axis=1,
-            result_type="expand",
-        ),
-        left_index=True,
-        right_index=True,
-        how="left",
-    )
+    # stops = stops.merge(
+    #     actual_stops.apply(
+    #         ft.partial(stop_properties, full_sl=stops, scope="system", space=space),
+    #         axis=1,
+    #         result_type="expand",
+    #     ),
+    #     left_index=True,
+    #     right_index=True,
+    #     how="left",
+    # )
 
     with pd.option_context("mode.use_inf_as_na", True):
         stops["relative_insertion_position"] = (
