4c64d3bca33e71c146ac7f801cd413b128b47a5e
Matthias Dahlmanns <m.dahlmanns@fz-juelich.de>
2022-05-09 16:00:11 +0200

    add doc string for namespace 'ridepy'

--- ridepy/cpp/ridepy/spaces.h
+++ ridepy/cpp/ridepy/spaces.h
@@ -1,6 +1,10 @@
 #ifndef SPACES_H
 #define SPACES_H
 
+/*! \namespace ridepy
+ * \brief The namespace that contains all specific types of the RidePy package.
+ */
+
 #include "../../util/spaces_cython/cspaces.h"
 
 #endif // SPACES_H
