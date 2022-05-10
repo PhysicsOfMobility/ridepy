6beb68c85930a7e0ef004a1aa398d6a26112d130
Matthias Dahlmanns <m.dahlmanns@fz-juelich.de>
2022-05-10 12:54:10 +0200

    add Doxygen documentation

--- ridepy/util/spaces_cython/cspaces.h
+++ ridepy/util/spaces_cython/cspaces.h
@@ -17,13 +17,47 @@
 using namespace std;
 
 namespace ridepy {
+
+/*!
+ * \addtogroup RidePy
+ * @{
+ */
+
+/*!
+ * \brief A point in the 2D plane
+ */
 typedef pair<double, double> R2loc;
 
-template <typename Loc> class TransportSpace {
+/*!
+ * \brief The abtract template for a TransportSpace within which ride-pooling vehicles can move
+ * \tparam Loc The measure that allows to identify a location in the transport space.
+ * This measure might for instance be a R2Loc to identify a location in the 2D Euclidian plane.
+ *
+ * Custom transport spaces can be defined by inheriting this interface.
+ *
+ * The type template parameter \p Loc expresses the measure, how to identify a location.
+ * This might be for instance a R2Loc to identify a location by its position in the 2D Euclidian plane or
+ * the unique id of a network node.
+ */
+template <typename Loc>
+class TransportSpace {
 public:
   double velocity;
 
+  /*!
+   * \brief Returns the spacial distance between the locations \p u and \p v inside this space
+   * \param u The starting point
+   * \param v The destination
+   * \return The spacial distance between the locations \p u and \p v
+   */
   virtual double d(Loc u, Loc v) = 0;
+
+  /*!
+   * \brief Returns the time needed to travel from location \p u to \p v inside this space
+   * \param u The starting point
+   * \param v The destination
+   * \return The time needed to travel from location \p u to \p v
+   */
   virtual double t(Loc u, Loc v) = 0;
   virtual pair<Loc, double> interp_dist(Loc u, Loc v, double dist_to_dest) = 0;
   virtual pair<Loc, double> interp_time(Loc u, Loc v, double time_to_dest) = 0;
@@ -33,6 +67,9 @@ public:
   virtual ~TransportSpace(){};
 };
 
+/*!
+ * \brief The Euclidean2D class allows vehicles to drive anywhere on the 2D plane.
+ */
 class Euclidean2D : public TransportSpace<R2loc> {
 public:
   double d(R2loc u, R2loc v) override;
@@ -46,6 +83,9 @@ public:
   Euclidean2D(double);
 };
 
+/*!
+ * \brief The Euclidean2D class allows vehicles to drive anywhere on the 2D plane.
+ */
 class Manhattan2D : public TransportSpace<R2loc> {
 public:
   double d(R2loc u, R2loc v) override;
@@ -59,7 +99,11 @@ public:
   Manhattan2D(double);
 };
 
-} // namespace cstuff
+/*!
+ * @}
+ */
+
+} // namespace ridepy
 
 #endif
 // RIDEPY_CSPACES_H
