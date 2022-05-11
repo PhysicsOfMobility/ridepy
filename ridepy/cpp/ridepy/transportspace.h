c670c68421548b2062937af12a93c4a676a622f3
Matthias Dahlmanns <m.dahlmanns@fz-juelich.de>
2022-05-11 09:56:47 +0200

    extract TransportSpace template to separate file

---
+++ ridepy/cpp/ridepy/transportspace.h
@@ -0,0 +1,6 @@
+#ifndef TRANSPORTSPACE_H
+#define TRANSPORTSPACE_H
+
+#include "../../util/spaces_cython/transportspace.h"
+
+#endif // TRANSPORTSPACE_H
--- ridepy/util/spaces_cython/cspaces.h
+++ ridepy/util/spaces_cython/cspaces.h
@@ -14,6 +14,8 @@
 #include <utility> // for pair
 #include <vector>
 
+#include "transportspace.h"
+
 using namespace std;
 
 namespace ridepy {
@@ -28,44 +30,6 @@ namespace ridepy {
  */
 typedef pair<double, double> R2loc;
 
-/*!
- * \brief The abtract template for a TransportSpace within which ride-pooling vehicles can move
- * \tparam Loc The measure that allows to identify a location in the transport space.
- * This measure might for instance be a R2Loc to identify a location in the 2D Euclidian plane.
- *
- * Custom transport spaces can be defined by inheriting this interface.
- *
- * The type template parameter \p Loc expresses the measure, how to identify a location.
- * This might be for instance a R2Loc to identify a location by its position in the 2D Euclidian plane or
- * the unique id of a network node.
- */
-template <typename Loc>
-class TransportSpace {
-public:
-  double velocity;
-
-  /*!
-   * \brief Returns the spacial distance between the locations \p u and \p v inside this space
-   * \param u The starting point
-   * \param v The destination
-   * \return The spacial distance between the locations \p u and \p v
-   */
-  virtual double d(Loc u, Loc v) = 0;
-
-  /*!
-   * \brief Returns the time needed to travel from location \p u to \p v inside this space
-   * \param u The starting point
-   * \param v The destination
-   * \return The time needed to travel from location \p u to \p v
-   */
-  virtual double t(Loc u, Loc v) = 0;
-  virtual pair<Loc, double> interp_dist(Loc u, Loc v, double dist_to_dest) = 0;
-  virtual pair<Loc, double> interp_time(Loc u, Loc v, double time_to_dest) = 0;
-
-  TransportSpace() : velocity{1} {};
-  TransportSpace(double velocity) : velocity{velocity} {};
-  virtual ~TransportSpace(){};
-};
 
 /*!
  * \brief The Euclidean2D class allows vehicles to drive anywhere on the 2D plane.
