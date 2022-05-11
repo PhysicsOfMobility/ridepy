cb177fbc9982cd5d415e193fed74948da2b46e9a
Matthias Dahlmanns <m.dahlmanns@fz-juelich.de>
2022-05-11 09:46:43 +0200

    add VehicleState to namespace ridepy and to lib

---
+++ ridepy/cpp/ridepy/vehiclestate.h
@@ -0,0 +1,6 @@
+#ifndef VEHICLESTATE_H
+#define VEHICLESTATE_H
+
+#include "../../vehicle_state_cython/cvehicle_state.h"
+
+#endif // VEHICLESTATE_H
--- ridepy/vehicle_state_cython/cvehicle_state.h
+++ ridepy/vehicle_state_cython/cvehicle_state.h
@@ -15,7 +15,7 @@
 #include <utility>
 
 using namespace std;
-namespace cstuff {
+namespace ridepy {
 
 struct StopEventSpec {
   StopAction action;
@@ -151,6 +151,7 @@ public:
 
   void select_new_stoplist() { stoplist = stoplist_new; }
 };
-} // namespace cstuff
+
+} // namespace ridepy
 
 #endif // RIDEPY_CVEHICLE_STATE_H
