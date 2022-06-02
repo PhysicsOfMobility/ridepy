b6ce4225b0221a91bed5b89905d22d208a5e6b0c
Matthias Dahlmanns <m.dahlmanns@fz-juelich.de>
2022-06-02 17:55:49 +0200

    setup main.cxx for flexible testings

--- ridepy/cpp/main.cxx
+++ ridepy/cpp/main.cxx
@@ -2,11 +2,26 @@
 using std::cout;
 using std::endl;
 
+#include <string>
+
 #include "tests/tests.h"
 
-int main() {
+int main(const int argc, char *argv[]) {
 
-    test_misc();
+    cout << "start RidePy C++ tests..." << endl;
+    // The test case that should be executed can be choosen using a command line argument.
+    // If no or an unknown mode name is specified, the test_misc() function will be called.
+    const std::string mode = argc>1 ? argv[1] : "";
+
+    cout << "using mode '" << mode << "'." << endl
+         << "---------------------------------" << endl;
+
+    if (mode == "simpleSquareGrid"){
+        test_simpleSquareGridSimulation();
+    // to include new test cases, just add another else if here
+    } else {
+        test_misc();
+    }
 
     return 0;
 }
